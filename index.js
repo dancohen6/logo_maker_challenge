4abaeaeba6ee7ed74ff55e67bb75d80483e67b85
dancohen6 <dzcsix@gmail.com>
2023-07-20 15:11:00 -0400

    everything working except for color

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const util = require('util');
-const inquirer = require('inquirer');
 const fs = require('fs');
 const Shape = require('./lib/shapes');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 const questions = [
     {
@@ -34,11 +39,25 @@ async function writeToFile(fileName, data) {
     }
 }
 
+function promptQuestion(question) {
+    return new Promise(resolve => {
+        rl.question(question.message, answer => {
+            resolve({ [question.name]: answer });
+        });
+    });
+}
+
 async function init() {
     try {
-        const data = await inquirer.prompt(questions);
-        const userLogo = new Shape(data.text, data.textcolor, data.shape, data.shapecolor);
+        const answers = {};
+        for (const question of questions) {
+            Object.assign(answers, await promptQuestion(question));
+        }
+
+        const userLogo = new Shape(answers.text, answers.textcolor, answers.shape, answers.shapecolor);
         await writeToFile('./examples/logo.svg', userLogo.render());
+
+        rl.close(); // Close the readline interface after all questions are answered
     } catch (err) {
         console.log(err);
     }
